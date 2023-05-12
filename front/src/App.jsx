de0cd95cc361e4fa4fda93410f20bd08ac98dff0
nnataali <natalia.charkviani.1@btu.edu.ge>
2023-05-12 22:10:10 +0400

    feat: project setup

---
+++ front/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+function App() {
+  return (
+    <>
+      <p className=' text-white bg-black'>mamma</p>
+      <button>njuy</button>
+    </>
+  );
+}
+
+export default App;
