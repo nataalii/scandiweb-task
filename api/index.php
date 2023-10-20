9a8ad2fa692d117f5280663eff2d47521438829a
Data <davit.charkviani.1@btu.edu.ge>
2023-10-20 19:29:10 +0200

    fix

--- api/index.php
+++ api/index.php
@@ -6,32 +6,21 @@ use app\Router;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$allowedOrigins = array(
-    'http://127.0.0.1:5173',
-    'http://localhost:8080',
-    'https://scandiweb-task-natali.netlify.app',
-    'https://scandiweb-natali.000webhostapp.com',
-
-);
-
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-}
+header("Access-Control-Allow-Origin: http://localhost:5173"); 
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    }
 
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
     exit(0);
 }
-
 
 $database = new Database();
 $conn = $database->connect();
