c085899228ddb07de3ecca1aca5f8431bd18f2e9
Data <davit.charkviani.1@btu.edu.ge>
2023-10-21 03:46:56 +0200

    fix

--- front/src/axios.js
+++ front/src/axios.js
@@ -4,8 +4,8 @@ const axiosInstance = axios.create({
   baseURL: 'https://scandiweb-natali.000webhostapp.com/',
   timeout: 5000,
   headers: {
-    Accept: 'application/json',
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
   withCredentials: true,
 });
