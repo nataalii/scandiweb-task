863f6a035993120f72a6825a309a4d853ba34064
Data <davit.charkviani.1@btu.edu.ge>
2023-10-21 03:32:55 +0200

    fix

--- front/src/pages/ProductAdd/useProductAdd.js
+++ front/src/pages/ProductAdd/useProductAdd.js
@@ -1,7 +1,6 @@
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../axios';
-import axios from 'axios';
 
 const useProductAdd = () => {
   const methods = useForm({ mode: 'all' });
@@ -9,10 +8,7 @@ const useProductAdd = () => {
   const onSubmit = async (data) => {
     console.log(data);
     try {
-      await axios.post(
-        'https://scandiweb-natali.000webhostapp.com/products/create',
-        data
-      );
+      await axiosInstance.post('products/create', data);
       navigate('/');
     } catch (err) {
       const error = err.response.data.message;
