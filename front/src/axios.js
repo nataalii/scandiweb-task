0fdc1dce35eb5d35657f2fcbba5e8be5fd75ea42
Data <davit.charkviani.1@btu.edu.ge>
2023-10-21 03:23:36 +0200

    fix url

--- front/src/axios.js
+++ front/src/axios.js
@@ -6,7 +6,6 @@ const axiosInstance = axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
   },
   withCredentials: true,
 });
