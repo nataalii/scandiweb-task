10a1ec2a161bf4ddd3eda39d05d62ffb1f22df8d
Data <davit.charkviani.1@btu.edu.ge>
2023-10-22 18:47:57 +0200

    fix

--- front/src/axios.js
+++ front/src/axios.js
@@ -2,12 +2,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://scandiweb--natali.000webhostapp.com',
-  timeout: 5000,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
-  httpVersion: '1.1',
 });
 
 export default axiosInstance;
